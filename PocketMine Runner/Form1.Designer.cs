db593ddb6f5bfbaa9b9fb9fb1b6986c2378c2b36
Glitchmaster-PE <lucasphillips712@gmail.com>
2013-11-27 02:44:12 -0500

    Update
    Added the raw files

---
+++ PocketMine Runner/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ PocketMine Runner/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button3.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (m_pocketMineProcess == null)
+            {
+                Cursor = Cursors.WaitCursor;
+                m_pocketMineProcess = new Process();
+                m_pocketMineProcess.StartInfo.CreateNoWindow = true;
+                m_pocketMineProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Minimized;
+                m_pocketMineProcess.StartInfo.WorkingDirectory = @"C:\Users\Lucas\Documents\Visual Studio 2013\Projects\WindowsFormsApplication1\WindowsFormsApplication1\PocketMine-MP";
+                m_pocketMineProcess.StartInfo.FileName = "bin\\php\\php.exe";
+                m_pocketMineProcess.StartInfo.Arguments = @"PocketMine-MP.php --enable-ansi %*";
+                //m_pocketMineProcess.StartInfo.Arguments = @"-o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o CursorType=0 -o CursorBlinks=1 -h error -t 'PocketMine-M' -i pocketmine.ico -w max php\php.exe -d enable_dl=On ..\PocketMine-MP.php --enable-ansi %*";
+                //m_pocketMineProcess.StartInfo.UseShellExecute = false;
+                //m_pocketMineProcess.StartInfo.RedirectStandardInput = true;
+                //m_pocketMineProcess.StartInfo.RedirectStandardOutput = true;
+
+                m_pocketMineProcess.Start();
+
+                System.Threading.Thread.Sleep(10000);
+
+
+                //Console.WriteLine(m_pocketMineProcess.StandardOutput.ReadToEnd());
+
+
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                UseWaitCursor = false;
+                Cursor = Cursors.Arrow;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (m_pocketMineProcess != null)
+            {
+                m_pocketMineProcess.Kill();
+                m_pocketMineProcess = null;
+                button1.Enabled = true;
+                button2.Enabled = false;
+                button3.Enabled = false;
+            }
+        }
+
+        private Process m_pocketMineProcess;
+
+
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (m_pocketMineProcess != null)
+            {
+                IntPtr pointer = m_pocketMineProcess.MainWindowHandle;
+                int status = SetForegroundWindow(pointer);
+                SendKeys.Send("Stop" + Environment.NewLine);
+                m_pocketMineProcess = null;
+            }
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (m_pocketMineProcess != null)
+            {
+                IntPtr pointer = m_pocketMineProcess.MainWindowHandle;
+                int status = SetForegroundWindow(pointer);
+                SendKeys.Send("Stop" + Environment.NewLine);
+                m_pocketMineProcess = null;
+                System.Threading.Thread.Sleep(1);
+                Cursor = Cursors.WaitCursor;
+                m_pocketMineProcess = new Process();
+                m_pocketMineProcess.StartInfo.CreateNoWindow = true;
+                m_pocketMineProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Minimized;
+                m_pocketMineProcess.StartInfo.WorkingDirectory = @"C:\Users\Lucas\Documents\Visual Studio 2013\Projects\WindowsFormsApplication1\WindowsFormsApplication1\PocketMine-MP";
+                m_pocketMineProcess.StartInfo.FileName = "bin\\php\\php.exe";
+                m_pocketMineProcess.StartInfo.Arguments = @"PocketMine-MP.php --enable-ansi %*";
+                //m_pocketMineProcess.StartInfo.Arguments = @"-o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o CursorType=0 -o CursorBlinks=1 -h error -t 'PocketMine-M' -i pocketmine.ico -w max php\php.exe -d enable_dl=On ..\PocketMine-MP.php --enable-ansi %*";
+                //m_pocketMineProcess.StartInfo.UseShellExecute = false;
+                //m_pocketMineProcess.StartInfo.RedirectStandardInput = true;
+                //m_pocketMineProcess.StartInfo.RedirectStandardOutput = true;
+
+                m_pocketMineProcess.Start();
+
+                System.Threading.Thread.Sleep(10000);
+
+
+                //Console.WriteLine(m_pocketMineProcess.StandardOutput.ReadToEnd());
+
+
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                UseWaitCursor = false;
+                Cursor = Cursors.Arrow;
+            }
+
+        }
+        }
+
+    
+}
+ 
